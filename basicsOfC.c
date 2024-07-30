ca40c56f3211f057680f0503a8fc7acc85dee5ad
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 <pedro.henrique.g.paula@gmail.com>
2024-07-29 22:33:33 -0300

    Changes

--- basicsOfC.c
+++ basicsOfC.c
@@ -30,15 +30,34 @@ int main(){
     //&& É o operador And
     // || é o operador or
     //! Not ( negação )
+    //Função ajuda a organizar o codigo e subdividir ele
 
+    //VETOREs/ARRAYS
 
-    //Laços de repetição 
+    //Para definir um vetor / array botamos entre colchetes o numero de casas/posições ele vai ter
 
-    
+    int vetor[4];
 
+    //Para pormos valor podemos por
 
+    vetor[0] = 23;
+    vetor[1] = 10;
+    vetor[2] = 5;
+    vetor[3] = 10;
 
+    //Para dgitar o numero que queremos botar de jeito mais facil podemos usar o for
+    //A cada numero do for a gente scan algo digitado
 
-    
+    //Matrizes é a mesma coisa que vetor, mas com mais dimensão, geralmente 2
+    //Matriz eu ja aprendi 
+
+    int matriz[3][3]; //Aqui criei uma matriz de 3 colunas e 3 linha, ou melhor 3 linhas e 3 colunas
+    //Mesma notação da matematica Xnxm
+    //Onde n é linhas e m colunas
+    //Para prencher podemos ir por indice que nem no exemplo de vetor
+    //Ou com duplo for um para linha outro para coluna
+    //Sendo o primeiro for == 0 significa que estamos na primeira linha
+    //o segundo for anda da coluna 0 até a coluna 3, etc...
+
     return 0;
 }
